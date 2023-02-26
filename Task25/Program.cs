89d5192faf84b9cc6f015575f033896079fb0967
NIKITA <Nikita.bort87@yandex.ru>
2023-02-26 21:03:52 +0300

    Home work 4

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите степень: ");
+int ex = Convert.ToInt32(Console.ReadLine());
+int Pow(int num, int ex)
+{
+    int num1 =1;
+    for(int i = 0; i < ex; i++)
+    {
+        num1 = num1*num;
+    }
+    return num1;
+}
+
+System.Console.WriteLine(Pow( num,  ex));
---
+++ Task29/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int Prompt(string message)
+{
+    System.Console.Write(message); 
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+//метод для получения случайных чисел массива 
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[]array = new int [Length];//обьявляем массив
+    Random random = new Random();
+    for(int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue +1);//заполняем случайными цифрами из диапазона SatArr до EndArr
+    }
+    return array;
+} 
+//Вывод значения масссива 
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, "); //вывод значения массива 
+    }
+    System.Console.Write($"{array[array.Length - 1]}"); // вывод значения массива 
+    System.Console.Write("]");
+}
+int length = Prompt("Длинна массива: ");
+int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+int max = Prompt("Конечное значение, для диапазона случайного числа: ");
+int [] array = GenerateArray(length, min, max);//Заполнение массива случайными числами
+PrintArray(array); //вывод массива
